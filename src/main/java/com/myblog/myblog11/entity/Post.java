59df65fbec3c14e1fdca04c79c83141ff079721b
rakeshshit <iamrakeshit60@gmail.com>
2024-12-27 02:55:02 +0530

    RKS : Entity class & database connection  add

---
+++ src/main/java/com/myblog/myblog11/entity/Post.java
@@ -0,0 +1,21 @@
+package com.myblog.myblog11.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String content;
+}
